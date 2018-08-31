39b4f297a1be10319e2648ac1bb5858843a774eb
Kevin Ross <kevin.ross@alienfast.com>
2018-08-31 15:18:40 -0500

    TS2307 with direct file imports (imports that bypass an index)

--- packages/pkg2/src/index.ts
+++ packages/pkg2/src/index.ts
@@ -1,5 +1,6 @@
-import * as p1 from '@ryancavanaugh/pkg1';
+// import * as p1 from '@ryancavanaugh/pkg1'; // works
+import { fn } from '@ryancavanaugh/pkg1/foo'; // does not work
 
 export function fn4() {
-  p1.fn();
+  fn();
 }
